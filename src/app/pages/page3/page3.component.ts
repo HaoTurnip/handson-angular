53327a280e67676580168e855234dc1a271e9353
Turnip <connection5bars@gmail.com>
2025-05-30 01:40:57 +0300

    Update page3.component.ts

--- src/app/pages/page3/page3.component.ts
+++ src/app/pages/page3/page3.component.ts
@@ -13,13 +13,13 @@ export class Page3Component implements OnInit {
     nickName: '',
     address: null as string | null
   };
-  
-  // this is a hack to make the nickname field work lol dont judge me
+
+
   displayNickname = '';
-  
-  private userSubject = new BehaviorSubject<any>(this.user);
+
+  private userSubject = new BehaviorSubject<any>({});
   userData$ = this.userSubject.asObservable();
-  
+
   submitted = false;
   jsonOutput = '';
   liveJsonOutput = '';
@@ -33,13 +33,18 @@ export class Page3Component implements OnInit {
   }
 
   updateUser() {
-    this.userSubject.next({...this.user});
+    const filteredUser = Object.fromEntries(
+      Object.entries(this.user)
+        .filter(([_, value]) => value !== '' && value !== null)
+    );
+
+    this.userSubject.next(filteredUser);
   }
 
   onFirstNameChange() {
     if (!this.hasCustomNickname) {
       this.displayNickname = this.user.firstName;
-      // Only update the actual user object if the field has been focused
+
       if (this.nicknameFocused) {
         this.user.nickName = this.user.firstName;
       }
@@ -47,22 +52,22 @@ export class Page3Component implements OnInit {
   }
 
   onNicknameChange() {
-    // If user typed something custom, set the flag
+
     if (this.displayNickname !== this.user.firstName) {
       this.hasCustomNickname = true;
     }
-    // If user cleared it, reset the flag
+
     if (!this.displayNickname) {
       this.hasCustomNickname = false;
     }
-    // Update the actual user object with the display value
+
     this.user.nickName = this.displayNickname;
     this.updateUser();
   }
-  
+
   onNicknameFocus() {
     this.nicknameFocused = true;
-    // When focused for the first time, sync the model with the display value
+
     if (!this.hasCustomNickname && !this.user.nickName && this.displayNickname) {
       this.user.nickName = this.displayNickname;
       this.updateUser();
@@ -70,12 +75,18 @@ export class Page3Component implements OnInit {
   }
 
   onSubmit() {
-    // Make sure the nickname is synced before submitting
+
     if (!this.user.nickName && this.displayNickname) {
       this.user.nickName = this.displayNickname;
     }
+
+    const filteredUser = Object.fromEntries(
+      Object.entries(this.user)
+        .filter(([_, value]) => value !== '' && value !== null)
+    );
+
     this.submitted = true;
-    this.jsonOutput = JSON.stringify(this.user, null, 2);
+    this.jsonOutput = JSON.stringify(filteredUser, null, 2);
   }
 
   resetForm() {
